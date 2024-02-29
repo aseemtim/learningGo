5d9b91c1055696df49d44ec6d930c548b7ef67bc
Aseem Timalsina <aseem.tim@gmail.com>
2024-02-28 22:58:32 -0500

    Add withdraw test case

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,14 +5,28 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-
-	wallet.Deposit(10)
-
-	got := wallet.Balance()
-	want := 10
-
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+		
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 }
