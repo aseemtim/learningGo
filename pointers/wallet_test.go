256dd507bae291570a3804595e95e8b1527f47c3
Aseem Timalsina <aseem.tim@gmail.com>
2024-02-28 23:50:37 -0500

    Refactor helper functions and completed errcheck

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -13,11 +13,11 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("withdraw", func(t *testing.T) {
+	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
 		
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
@@ -48,5 +48,11 @@ func assertError(t testing.TB, got, want error) {
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
