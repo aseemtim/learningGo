b4bf37511684ad722f2f507e5fc22bfdc4edbdd2
Aseem Timalsina <aseem.tim@gmail.com>
2024-02-29 21:53:25 -0500

    Add and refactor test case for adding a new key function

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,12 +3,32 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
-	
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	assertStrings(t, got, want)
+	t.Run("known word", func(t *testing.T){
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unkown word", func(t *testing.T){
+		_, got := dictionary.Search("unkown")
+
+		// if err == nil {
+		// 	t.Fatal("exptected to get an error")
+		// }
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
 }
 
 func assertStrings(t testing.TB, got, want string) {
@@ -18,3 +38,20 @@ func assertStrings(t testing.TB, got, want string) {
 	}
 }
 
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got,err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	assertStrings(t, got, definition)	
+}
